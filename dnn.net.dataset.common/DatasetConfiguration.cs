b6724624c49824a62d9b616f1983356ca4273d02
ZoroDerVonCodier <daveb@signalpop.com>
2020-02-23 20:24:21 -0800

    Added LoadNext with datetime.

--- dnn.net.dataset.common/DatasetConfiguration.cs
+++ dnn.net.dataset.common/DatasetConfiguration.cs
@@ -88,6 +88,7 @@ namespace DNN.net.dataset.common
         byte[] LoadData(int nDatasetID, DateTime dtStart, DateTime dtEnd, Log log = null, Dictionary<string, string> rgParam = null);
         byte[] LoadData(int nDatasetID, DateTime dt, int nCount = -1, Log log = null, Dictionary<string, string> rgParam = null);
         byte[] LoadNext(int nSteps);
+        byte[] LoadNext(DateTime dt);
         Bitmap GetCurrentImage(out DateTime dt, out bool bBadImage);
         string GetConfigurationSettings();
     }
