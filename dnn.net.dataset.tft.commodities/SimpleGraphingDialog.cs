973a99b4d686b0a0baf79bac72bbd22c37ceb843
Dave Brown <daveb@signalpop.com>
2024-07-26 20:01:39 -0700

    Upgraded to Entity Frameworks 6.5.1

---
+++ dnn.net.dataset.tft.commodities/SimpleGraphingDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleGraphing;
+
+namespace SimpleGraphing
+{
+    public partial class SimpleGraphingDialog : Form
+    {
+        public SimpleGraphingDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void SimpleGraphingDialog_Load(object sender, EventArgs e)
+        {
+            PlotCollection plots = new PlotCollection();
+
+            for (int i = 0; i < 200; i++)
+            {
+                double dfY = Math.Sin(i/10.0);
+                double dfX = i;
+
+                plots.Add(dfX, dfY);
+            }
+
+            pictureBox1.Image = SimpleGraphingControl.QuickRender(plots, pictureBox1.Width, pictureBox1.Height);
+        }
+    }
+}
